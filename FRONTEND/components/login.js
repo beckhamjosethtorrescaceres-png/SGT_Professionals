44081d94c8ba7bd60cf2530fbd8fbbe0de5b1b06
isai <isaicatano1@gmail.com>
2026-06-03 15:00:11 -0500

    validacion login

---
+++ FRONTEND/components/login.js
@@ -0,0 +1,47 @@
+export function validar() {
+    const form = document.getElementById("login-form")
+
+    form.addEventListener("submit", async (e) =>{
+        e.preventDefault()
+
+        const email = document.getElementById("email").value
+        const password = document.getElementById("password").value
+
+        if (!email || !password) {
+        alert("Todos los campos son obligatorios");
+        return;
+        }
+
+        if (!email.includes("@")) {
+        alert("Ingresa un correo válido");
+        return;
+        }
+
+        if (password.length < 6) {
+        alert("La contraseña debe tener al menos 6 caracteres");
+        return;
+        }
+
+        const response = await fetch("BACKEND/auth-db.json");
+        const data = await response.json();
+        const user = data.users.find(u => u.email === email && u.password === password);
+
+        if(user){
+            localStorage.setItem("user", JSON.stringify(user))
+            vistas(user.rol)
+        }else{
+            alert("credenciales incorrectas")
+        }
+
+        form.reset()
+    })
+}
+
+
+export function vistas (rol) {
+    if(rol === "admin"){
+        window.location.href = "index.html"
+    }else if(rol === "tecnico"){
+        window.location.href =""
+    }
+}
